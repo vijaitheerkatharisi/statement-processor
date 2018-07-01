37f720fe72142b3fbfcee90b2bfe18984e823da7
VIJAI-PC\vijai <vijai.delta@gmail.com>
2018-07-01 16:08:04 +0530

    documentation update

--- src/main/java/com/rabobank/statement/constants/Documentation.java
+++ src/main/java/com/rabobank/statement/constants/Documentation.java
@@ -5,5 +5,5 @@ public class Documentation {
 	private Documentation(){
 		//
 	}
-	public static final String STATMENT_PROCESS_CSV_AND_XML_NOTES="This end process accept only xml and CSV statement transaction records and display both the transaction reference and description of each of the failed records";
+	public static final String STATMENT_PROCESS_CSV_AND_XML_NOTES="This endpoint process accept only xml and CSV statement transaction records and display the transaction reference and description of each of the failed records";
 }
